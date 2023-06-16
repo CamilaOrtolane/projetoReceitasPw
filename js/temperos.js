89ec205998d327d0fa6da77590483bc26b495c1e
CamilaOrtolane <camilaortolane13@gmail.com>
2023-06-16 15:47:31 -0400

    HAHAH

--- js/temperos.js
+++ js/temperos.js
@@ -1,23 +1,7 @@
-const form = document.getElementById('form');
-const campos = document.querySelectorAll('required');
-const spans = document.querySelectorAll('span-required');
+function validar(){
+    var nome = document.getElementById("nome");
+    if(nome.value === ""){
+        alert("Digite o nome do tempero")
 
-function setError(temperos){
-    campos [index].computedStyleMap.border = '2px solid #e63636';
-    spans[index].computedStyleMap.display = 'block';
-
-}
-
-function removeError(index){
-    campos [index].computedStyleMap.border = '';
-    spans[index].computedStyleMap.display = 'none';
-}
-
-function nameValidate(){
-    if(campos[0].ariaValueMax.length < 3){
-        setError(0);
-    }
-    else{
-        console.log('NOME VALIDADO');
     }
 }
